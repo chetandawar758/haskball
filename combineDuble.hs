9594a1589fcf772a6aba5f7f8f4c833a4da98f0d
ROHIT CHOUHAN <43743224+rchouhan170590@users.noreply.github.com>
2019-03-30 22:53:52 +0530

    Create combineDuble.hs

---
+++ combineDuble.hs
@@ -0,0 +1,20 @@
+
+--Input is like :"aabbcdaaddeffg"
+-- Output is : ["aa","bb","c","d","aa","dd","e","ff","g"]
+removeduble :: [Char]->[Char]
+removeduble [] = []
+removeduble (x:xs) = x:removeduble(dropWhile (==x) xs)
+
+
+run :: [Char]->[Char]->[Char]
+run [] ys = []
+run  xs ys = if((head xs)==(head ys))
+             then 
+                (head xs):run (tail xs) ys
+             else
+                ' ':(head xs):run (tail xs) (tail ys) 
+
+
+
+combineDuble :: [Char]->[[Char]]
+combineDuble xs = words(run xs (combineDuble xs))
